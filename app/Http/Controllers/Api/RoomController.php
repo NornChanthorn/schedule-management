2d267588fc02da30ae36a490fbab938df69c2bce
Chichhorng Koem <chichhorng.koem@student.cadt.edu.kh>
2024-01-31 15:04:42 +0700

    API

---
+++ app/Http/Controllers/Api/RoomController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Room;
+use Illuminate\Http\Request;
+
+class RoomController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $rooms = Room::all();
+        return response()->json($rooms);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string',
+            'size' => 'required|integer',
+            'type' => 'required|string',
+            'status' => 'required|string',
+            // 'status' => 'boolean',
+        ]);
+
+        $room = Room::create($validatedData);
+
+        return response()->json($room, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Room  $room
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Room $room)
+    {
+        return response()->json($room);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Room  $room
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Room $room)
+    {
+        $validatedData = $request->validate([
+            'name' => 'string',
+            'size' => 'integer',
+            'type' => 'string',
+            'status' => 'string',
+        ]);
+
+        $room->update($validatedData);
+
+        return response()->json($room, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Room  $room
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Room $room)
+    {
+        $room->delete();
+
+        return response()->json(null, 204);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
@@ -11,3 +12,17 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+
+
+// routes/api.php
+
+use App\Http\Controllers\Api\RoomController;
+use App\Http\Controllers\Api\TermController;
+use App\Http\Controllers\Api\MajorController;
+use App\Http\Controllers\Api\GenerationController;
+
+Route::apiResource('rooms', RoomController::class);
+Route::apiResource('terms', TermController::class);
+Route::apiResource('majors', MajorController::class);
+Route::apiResource('generations', GenerationController::class);
