b0b72858c5ef339f5f352b12b4176c64482e698f
Developer 4 <dev4@poscardigital.com>
2024-02-21 11:40:09 +0700

    course edit

--- resources/js/components/admin/Generation.vue
+++ resources/js/components/admin/Generation.vue
@@ -2,7 +2,7 @@
    <Toast/>
     <div class="flex items-center mb-4 ml-4">
         <h1 class="text-custom-color-small font-istok text-4xl font-bold"> {{ majorName1 }}</h1>
-        <button class="ml-auto bg-blue-500 text-white px-2 py-2 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 mr-2" label="Add New" severity="secondary" @click="showDialog">
+        <button class="ml-auto bg-blue-500 text-white px-2 py-2 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 mr-2" label="Add New" severity="secondary" @click="showDialog">
             <span class="flex items-center">
             <i class="fa-solid fa-circle-plus mr-2"></i>
                 Add More
